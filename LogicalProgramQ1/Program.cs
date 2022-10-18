6f3e5932f00002853df81c631b6e169f9d4163aa
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-10-18 02:09:16 -0400

    Added file

--- LogicalProgramQ1/Program.cs
+++ LogicalProgramQ1/Program.cs
@@ -4,11 +4,11 @@
     {
         static void Main(string[] args)
         {
-            int n1 = 0, n2 = 1, n3, i, number;
+            int n1 = 0, n2 = 1, n3, i, num;
             Console.Write("Enter the number of elements: ");
-            number = int.Parse(Console.ReadLine());
+            num = int.Parse(Console.ReadLine());
             Console.Write(n1 + " " + n2 + " ");
-            for (i = 2; i < number; i++)
+            for (i = 2; i < num; i++)
             {
                 n3 = n1 + n2;
                 Console.Write(n3 + " ");
