73090bcab5bf307cff0238b7deb2818852887e50
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-10-18 02:18:40 -0400

    Added file

--- LogicalProgramQ1/Program.cs
+++ LogicalProgramQ1/Program.cs
@@ -4,35 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a number");
-            int number = Convert.ToInt32(Console.ReadLine());
-            int result = Check_Prime(number);
-            if (result == 0)
-            {
-                Console.WriteLine("{0} is not a prime number", number);
-            }
-            else
-            {
-                Console.WriteLine("{0} is  a prime number", number);
-            }
-            Console.Read();
-        }
+            Console.WriteLine("Please Enter Your Number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
 
-        private static int Check_Prime(int number)
-        {
-            int i;
-            for (i = 2; i <= number - 1; i++)
+            for (int i = n; i >= 1; i--)
             {
-                if (number % i == 0)
-                {
-                    return 0;
-                }
+                Console.WriteLine(i);
             }
-            if (i == number)
-            {
-                return 1;
-            }
-            return 0;
         }
     }
 }
