c427748ee8c9aa1e042aabbd546fe11a33d61637
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-10-22 02:36:11 -0400

    aaa

--- LogicalProgramQ1/Program.cs
+++ LogicalProgramQ1/Program.cs
@@ -2,6 +2,7 @@
 
 internal class Temperature
 {
+<<<<<<< Updated upstream
     public int Value;
     public Temperature(int value)
     {
@@ -11,22 +12,42 @@ internal class Temperature
     public void temperatureConversion()
     {
         switch (Value)
+=======
+public static  int Value;
+        public Temperature(int value)
+>>>>>>> Stashed changes
         {
-            case 1:
-                Console.WriteLine("Please Enter the Value to Convert Tempearture into Celcius: ");
-                int a = Convert.ToInt32(Console.ReadLine());
-                int result1 = (a * 9 / 5) + 32;
-                Console.WriteLine("Celcius Value is:" + result1);
-                break;
-            case 2:
-                Console.WriteLine("Please Enter the Value to Convert Tempearture into Fahrenheit: ");
-                int b = Convert.ToInt32(Console.ReadLine());
-                int result2 = (b - 32) * 5 / 9;
-                Console.WriteLine("Fahrenheit Value is:" + result2);
-                break;
+            Value = value;
+        }
 
+        public static void temperatureConversion()
+        {
+            Console.WriteLine("Please Enter Value : ");
+            int value = Convert.ToInt32(Console.ReadLine());
+
+            switch (value)
+            {
+                case 1:
+                    Console.WriteLine("Please Enter the Value to Convert Tempearture into Celcius: ");
+                    int a=Convert.ToInt32(Console.ReadLine());
+                    int result1 = (a * 9 / 5) + 32;
+                    Console.WriteLine("Celcius Value is:"+ result1);
+                    break;
+                case 2:
+                    Console.WriteLine("Please Enter the Value to Convert Tempearture into Fahrenheit: ");
+                    int b = Convert.ToInt32(Console.ReadLine());
+                    int result2 = (b -32) *5/9;
+                    Console.WriteLine("Fahrenheit Value is:" + result2);
+                    break;
+                   
+            }
+        }
+        static void Main(string[] args)
+        {
+            Temperature.temperatureConversion();
         }
     }
+<<<<<<< Updated upstream
     static void Main(string[] args)
     {
         Console.WriteLine("Please Enter Value : ");
@@ -37,5 +58,7 @@ internal class Temperature
 
     }
 }
+=======
+>>>>>>> Stashed changes
 }
     
